490f97d8eb9664260990eeaefaacef40ba358640
sheeran.xie <sheeran.xie@batechworks.com>
2025-02-28 20:49:05 +0800

    constructor issue of ohos video_player
    Signed-off-by: sheeran.xie <240660920@qq.com>
    
    (cherry picked from commit 23984686b5d83dd69d5739d4394166a54dfb7ced)

--- packages/video_player/video_player_ohos/lib/src/ohos_video_player.dart
+++ packages/video_player/video_player_ohos/lib/src/ohos_video_player.dart
@@ -20,6 +20,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:video_player_platform_interface/video_player_platform_interface.dart';
 
+import 'video_player_ohos_channel.dart';
 import 'messages.g.dart';
 
 /// An Android implementation of [VideoPlayerPlatform] that uses the
@@ -60,7 +61,8 @@ class OhosVideoPlayer extends VideoPlayerPlatform {
         httpHeaders = dataSource.httpHeaders;
         break;
       case DataSourceType.file:
-        uri = dataSource.uri;
+        int fileFd = await VideoPlayerOhosChannel.getFileFdByPath(dataSource.uri);
+        uri = 'fd://$fileFd';
         httpHeaders = dataSource.httpHeaders;
         break;
       case DataSourceType.contentUri:
