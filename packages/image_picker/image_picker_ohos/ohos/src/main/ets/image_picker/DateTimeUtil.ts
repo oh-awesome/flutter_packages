8c613c328f8acc4bc20e1610a847c675f17688ce
代青飞 <daiqinghui2@h-partners.com>
2025-03-03 16:57:11 +0800

    fix: GallerySaver.saveVideo保存了两张不同的图片，安卓仅一张。将image_picker录制的视频保存到缓存目录下。
    Signed-off-by: 代青飞 <daiqinghui2@h-partners.com>

--- packages/image_picker/image_picker_ohos/ohos/src/main/ets/image_picker/DateTimeUtil.ts
+++ packages/image_picker/image_picker_ohos/ohos/src/main/ets/image_picker/DateTimeUtil.ts
@@ -1,8 +1,6 @@
-/*
- * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
- * Use of this source code is governed by a MIT license that can be
- * found in the LICENSE file.
- */
+// Copyright (c) 2023 Hunan OpenValley Digital Industry Development Co., Ltd.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE_ODID file.
 
 /**
  * @file 日期工具
