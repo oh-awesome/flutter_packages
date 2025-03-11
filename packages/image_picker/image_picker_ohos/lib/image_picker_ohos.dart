2fd0cb8fe0f8d2cf06848ad0cc0176aa6f27149b
代青飞 <daiqinghui2@h-partners.com>
2025-03-11 10:24:54 +0800

    优化拍照保存两张的bug，回退视频选择播放的修改
    Signed-off-by: 代青飞 <daiqinghui2@h-partners.com>
    (cherry picked from commit bdad38910c6d59794d30d16759fdd02153befdf5)

--- packages/image_picker/image_picker_ohos/lib/image_picker_ohos.dart
+++ packages/image_picker/image_picker_ohos/lib/image_picker_ohos.dart
@@ -11,7 +11,6 @@ class ImagePickerOhos extends ImagePickerPlatform {
       : _hostApi = api ?? ImagePickerApi();
 
   final ImagePickerApi _hostApi;
-  final Map<String?, int?> fileFdlist = new Map();
   bool useOhosPhotoPicker = false;
   static void registerWith() {
     ImagePickerPlatform.instance = ImagePickerOhos();
@@ -57,7 +56,7 @@ class ImagePickerOhos extends ImagePickerPlatform {
     double? maxWidth,
     double? maxHeight,
     int? imageQuality,
-  }) async {
+  }) {
     if (imageQuality != null && (imageQuality < 0 || imageQuality > 100)) {
       throw ArgumentError.value(
           imageQuality, 'imageQuality', 'must be between 0 and 100');
@@ -71,7 +70,7 @@ class ImagePickerOhos extends ImagePickerPlatform {
       throw ArgumentError.value(maxHeight, 'maxHeight', 'cannot be negative');
     }
 
-    final List<String?> path = await _hostApi.pickImages(
+    return _hostApi.pickImages(
       SourceSpecification(type: SourceType.gallery),
       ImageSelectionOptions(
           maxWidth: maxWidth,
@@ -80,14 +79,6 @@ class ImagePickerOhos extends ImagePickerPlatform {
       GeneralOptions(
           allowMultiple: true, usePhotoPicker: useOhosPhotoPicker),
     );
-
-    List<String?> pathList = [];
-
-    for(int i = 0 ; i<path.length; i+=2){
-      pathList.add(path[i]);
-    }
-
-    return pathList;
   }
 
   Future<String?> _getImagePath({
@@ -152,15 +143,7 @@ class ImagePickerOhos extends ImagePickerPlatform {
         usePhotoPicker: useOhosPhotoPicker,
       ),
     );
-    fileFdlist[paths.first] = int.parse(paths[1] ?? '0');
     return paths.isEmpty ? null : paths.first;
-  }
-
-  int getFileFd(String? file) {
-    if(file == null){
-      return 0;
-    }
-    return fileFdlist[file]!;
   }
 
   @override
@@ -219,20 +202,15 @@ class ImagePickerOhos extends ImagePickerPlatform {
   Future<List<XFile>> getMedia({
     required MediaOptions options,
   }) async {
-    final List<String?> paths = await _hostApi.pickMedia(
+    return (await _hostApi.pickMedia(
       _mediaOptionsToMediaSelectionOptions(options),
       GeneralOptions(
         allowMultiple: options.allowMultiple,
         usePhotoPicker: useOhosPhotoPicker,
       ),
-    );
-    final List<String?> pathList = [];
-    for(int i = 1; i< paths.length; i+=2){
-      fileFdlist[paths[i-1]] = int.parse(paths[i] ?? '0');
-      pathList.add(paths[i - 1]);
-    }
-        
-    return pathList.map((String? path) => XFile(path!)).toList();
+     ))
+    .map((String? path) => XFile(path!))
+    .toList();
   }
 
   @override
