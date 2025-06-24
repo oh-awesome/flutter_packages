c1c703ef730b6c02c1c129aa0b60505f535c6f1d
xiaolieask <760956257@qq.com>
2025-06-24 14:35:21 +0800

    修复不绑定webview的情况下，controller无法获取useragen信息
    Signed-off-by: xiaolieask <760956257@qq.com>

--- packages/webview_flutter/webview_flutter/example/integration_test/webview_flutter_test.dart
+++ packages/webview_flutter/webview_flutter/example/integration_test/webview_flutter_test.dart
@@ -183,11 +183,23 @@ Future<void> main() async {
     await expectLater(buttonTapResizeCompleter.future, completes);
   });
 
-  testWidgets('set custom userAgent', (WidgetTester tester) async {
-    final Completer<void> pageFinished = Completer<void>();
+  testWidgets('No webview, get controller', (WidgetTester tester) async {
 
     final WebViewController controller = WebViewController();
+
+    final String? customUserAgent = await controller.getUserAgent();
+
+    expect(customUserAgent, startsWith('Mozilla'));
+
+  });
+
+  testWidgets('set custom userAgent', (WidgetTester tester) async {
+    final Completer<void> pageFinished = Completer<void>();
+    
+    final WebViewController controller = WebViewController();
+
     unawaited(controller.setJavaScriptMode(JavaScriptMode.unrestricted));
+
     unawaited(controller.setNavigationDelegate(NavigationDelegate(
       onPageFinished: (_) => pageFinished.complete(),
     )));
