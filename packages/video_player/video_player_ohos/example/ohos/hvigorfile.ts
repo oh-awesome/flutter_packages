7ffeadffedb524ab52c66d5de8511ec8a4a684fa
Liyupeng <liyupeng@micros.email>
2025-07-11 09:30:05 +0800

    feat:新增video_player2.10适配鸿蒙模板工程
    Signed-off-by: Liyupeng <liyupeng@micros.email>

---
+++ packages/video_player/video_player_ohos/example/ohos/hvigorfile.ts
@@ -0,0 +1,11 @@
+// Copyright (c) 2025 Huawei Device Co., Ltd.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE_HW file.
+// Based on Camera.java originally written by
+// Copyright 2013 The Flutter Authors.
+import { appTasks } from '@ohos/hvigor-ohos-plugin';
+
+export default {
+    system: appTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
+    plugins:[]         /* Custom plugin to extend the functionality of Hvigor. */
+}
