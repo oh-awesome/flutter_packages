f0fb629fbe73c1cf9a1f209a78b5da849980d713
Kyrei_Irving <tangbiteng25973@openvalley.net>
2025-07-31 15:56:14 +0800

    feat:升级为0.11.1
    Signed-off-by: Kyrei_Irving <tangbiteng25973@openvalley.net>

--- packages/camera/camera_ohos/lib/src/type_conversion.dart
+++ packages/camera/camera_ohos/lib/src/type_conversion.dart
@@ -13,14 +13,13 @@ CameraImageData cameraImageFromPlatformData(Map<dynamic, dynamic> data) {
       format: _cameraImageFormatFromPlatformData(data['format']),
       height: data['height'] as int,
       width: data['width'] as int,
-      lensAperture: double.parse(data['lensAperture'].toString()),
+      lensAperture: data['lensAperture'] as double?,
       sensorExposureTime: data['sensorExposureTime'] as int?,
-      sensorSensitivity: double.parse(data['sensorSensitivity'].toString()),
+      sensorSensitivity: data['sensorSensitivity'] as double?,
       planes: List<CameraImagePlane>.unmodifiable(
           (data['planes'] as List<dynamic>).map<CameraImagePlane>(
-                  (dynamic planeData) =>
-                  _cameraImagePlaneFromPlatformData(
-                      planeData as Map<dynamic, dynamic>))));
+              (dynamic planeData) => _cameraImagePlaneFromPlatformData(
+                  planeData as Map<dynamic, dynamic>))));
 }
 
 CameraImageFormat _cameraImageFormatFromPlatformData(dynamic data) {
