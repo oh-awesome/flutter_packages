73d3bd11b16441e9ae650b63ae0f008460dcc052
Kyrei_Irving <tangbiteng25973@openvalley.net>
2025-08-22 13:59:26 +0800

    fix:添加fileFd方法
    Signed-off-by: Kyrei_Irving <tangbiteng25973@openvalley.net>

--- packages/video_player/video_player/lib/video_player.dart
+++ packages/video_player/video_player/lib/video_player.dart
@@ -341,6 +341,7 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
   ///
   /// This will load the file from a file:// URI constructed from [file]'s path.
   /// [httpHeaders] option allows to specify HTTP headers, mainly used for hls files like (m3u8).
+  /// This is not supported on ohos, use fileFd below instead.
   VideoPlayerController.file(
     File file, {
     Future<ClosedCaptionFile>? closedCaptionFile,
@@ -353,6 +354,23 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
        package = null,
        formatHint = null,
        super(const VideoPlayerValue(duration: Duration.zero));
+
+  /// Constructs a [VideoPlayerController] playing a video from a file.
+  ///
+  /// This will load the file from a fd://fileFd constructed from file picker.
+  /// This is supported on ohos only.
+  VideoPlayerController.fileFd(
+    int fileFd,{
+        Future<ClosedCaptionFile>? closedCaptionFile,
+        this.videoPlayerOptions,
+        this.httpHeaders = const <String, String>{},
+        this.viewType = VideoViewType.textureView,
+    }) : _closedCaptionFileFuture = closedCaptionFile,
+        dataSource = "fd://$fileFd",
+        dataSourceType = DataSourceType.file,
+        package = null,
+        formatHint = null,
+        super(const VideoPlayerValue(duration: Duration.zero));
 
   /// Constructs a [VideoPlayerController] playing a video from a contentUri.
   ///
