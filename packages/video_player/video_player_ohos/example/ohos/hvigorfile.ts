5e044921e3b8345aa26fb8d986f742306eafad5f
Kyrei_Irving <tangbiteng25973@openvalley.net>
2025-10-11 16:29:15 +0800

    feat:新增readme、OAT文件，修改版权头
    Signed-off-by: Kyrei_Irving <tangbiteng25973@openvalley.net>

--- packages/video_player/video_player_ohos/example/ohos/hvigorfile.ts
+++ packages/video_player/video_player_ohos/example/ohos/hvigorfile.ts
@@ -1,8 +1,3 @@
-// Copyright (c) 2025 Huawei Device Co., Ltd.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE_HW file.
-// Based on Camera.java originally written by
-// Copyright 2013 The Flutter Authors.
 import { appTasks } from '@ohos/hvigor-ohos-plugin';
 
 export default {
