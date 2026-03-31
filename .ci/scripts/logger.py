86e848aed0dd094290609c0486f13cf1a23ace7c
qq_77777 <qishijie3@h-partners.com>
2026-03-31 19:16:10 +0800

    修改代码格式
    Signed-off-by: qq_77777 <qishijie3@h-partners.com>

--- .ci/scripts/logger.py
+++ .ci/scripts/logger.py
@@ -1,27 +1,27 @@
- # Copyright (c) 2025 Huawei Device Co., Ltd. All rights reserved.
- 	 # Use of this source code is governed by a BSD-style license that can be
- 	 # found in the LICENSE_HW file.
- 	 
- 	 import sys
- 	 
- 	 
- 	 class Logger:
- 	     """Simple logging functions"""
- 	 
- 	     @staticmethod
- 	     def info(message: str):
- 	         print(f"[INFO] {message}", flush=True)
- 	 
- 	     @staticmethod
- 	     def warn(message: str):
- 	         print(f"[WARN] {message}", flush=True)
- 	 
- 	     @staticmethod
- 	     def error(message: str):
- 	         print(f"[ERROR] {message}", flush=True)
- 	 
- 	     @staticmethod
- 	     def step(message: str):
- 	         print(f"\n{'=' * 40}", flush=True)
- 	         print(message, flush=True)
- 	         print(f"{'=' * 40}\n", flush=True)
+# Copyright (c) 2025 Huawei Device Co., Ltd. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE_HW file.
+
+import sys
+
+
+class Logger:
+    """Simple logging functions"""
+
+    @staticmethod
+    def info(message: str):
+        print(f"[INFO] {message}", flush=True)
+
+    @staticmethod
+    def warn(message: str):
+        print(f"[WARN] {message}", flush=True)
+
+    @staticmethod
+    def error(message: str):
+        print(f"[ERROR] {message}", flush=True)
+
+    @staticmethod
+    def step(message: str):
+        print(f"\n{'=' * 40}", flush=True)
+        print(message, flush=True)
+        print(f"{'=' * 40}\n", flush=True)
