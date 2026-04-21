b8a977d87cee78cc202f8a33c2f60fdd4bac85a3
zhong_ning <zhong_ning@hoperun.com>
2026-04-21 15:12:21 +0800

    change the video playback to read from a specific path
    Signed-off-by: zhong_ning <zhong_ning@hoperun.com>

--- packages/camera/camera_ohos/example/lib/main.dart
+++ packages/camera/camera_ohos/example/lib/main.dart
@@ -226,20 +226,20 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
 
   /// Display the thumbnail of the captured image or video.
   Widget _thumbnailWidget() {
-    QRCaptureController _controller = QRCaptureController();
+ 	  final VideoPlayerController? localVideoController = videoController;
     return Expanded(
       child: Align(
         alignment: Alignment.centerRight,
         child: Row(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            if (!isStartVideoPlayer  && imageFile == null )
+ 	          if (localVideoController == null && imageFile == null)
               Container()
             else
               SizedBox(
                 width: 64.0,
                 height: 64.0,
-                child: (!isStartVideoPlayer || imageFile != null)
+ 	              child: (localVideoController == null)
                     ? (
                     // The captured image on the web contains a network-accessible URL
                     // pointing to a location within the browser. It may be displayed
@@ -247,12 +247,15 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
                     // bytes to memory.
                     kIsWeb
                         ? Image.network(imageFile!.path)
-                        : QRCaptureView(controller: _controller))
+ 	                      : Image.file(File(imageFile!.path)))
                     : Container(
                     decoration: BoxDecoration(
                         border: Border.all(color: Colors.pink)),
                     child: Center(
-                        child: QRCaptureView(controller: _controller)
+                        child: AspectRatio(
+                            aspectRatio:
+                            localVideoController.value.aspectRatio,
+                            child: VideoPlayer(localVideoController)),
                     )      ),
               ),
           ],
@@ -741,6 +744,8 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
       if (mounted) {
         setState(() {
           imageFile = file;
+ 	  	    videoController?.dispose();
+ 	  	    videoController = null;
           Singleton.getInstance().setData(imageFile?.path ?? '');
         });
         if (file != null) {
@@ -1022,8 +1027,32 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
   }
 
   Future<void> _startVideoPlayer() async {
-    isStartVideoPlayer = true;
-    setState((){});
+ 	  if (videoFile == null) {
+ 	    return;
+ 	  }
+ 	  final VideoPlayerController vController;
+ 	   vController = kIsWeb
+ 	       ? VideoPlayerController.network(videoFile!.path)
+ 	       : VideoPlayerController.file(File(videoFile!.path));
+ 	  videoPlayerListener = () {
+ 	    if (videoController != null) {
+ 	      if (mounted) {
+ 	        setState(() {});
+ 	      }
+ 	      videoController!.removeListener(videoPlayerListener!);
+ 	    }
+ 	  };
+ 	  vController.addListener(videoPlayerListener!);
+ 	  await vController.setLooping(true);
+ 	  await vController.initialize();
+ 	  await videoController?.dispose();
+ 	  if (mounted) {
+ 	    setState(() {
+ 	      imageFile = null;
+ 	      videoController = vController;
+ 	    });
+ 	  }
+ 	  await vController.play();
   }
 
   Future<XFile?> takePicture() async {
