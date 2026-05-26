06157da59775099437239ec8add61c3411c8d846
xileilei <xi_leilei@hoperun.com>
2026-05-26 19:46:58 +0800

    回退集成测试和单元测试，保持和上游源库android插件里的单元测试和集成测试一致
    Signed-off-by: xileilei <xi_leilei@hoperun.com>

--- packages/url_launcher/url_launcher_ohos/test/url_launcher_ohos_test.dart
+++ packages/url_launcher/url_launcher_ohos/test/url_launcher_ohos_test.dart
@@ -63,7 +63,7 @@ void main() {
     });
   });
 
-  group('launch without webview', () {
+  group('legacy launch without webview', () {
     test('calls through', () async {
       final UrlLauncherOhos launcher = UrlLauncherOhos(api: api);
       final bool launched = await launcher.launch(
@@ -75,6 +75,7 @@ void main() {
         universalLinksOnly: false,
         headers: const <String, String>{},
       );
+
       expect(launched, true);
       expect(api.usedWebView, false);
       expect(api.passedWebViewOptions?.headers, isEmpty);
@@ -99,7 +100,7 @@ void main() {
       final UrlLauncherOhos launcher = UrlLauncherOhos(api: api);
       await expectLater(
           launcher.launch(
-            'noactivity://',
+            'https://noactivity',
             useSafariVC: false,
             useWebView: false,
             enableJavaScript: false,
@@ -127,7 +128,7 @@ void main() {
     });
   });
 
-  group('launch with webview', () {
+  group('legacy launch with webview', () {
     test('calls through', () async {
       final UrlLauncherOhos launcher = UrlLauncherOhos(api: api);
       final bool launched = await launcher.launch(
@@ -141,6 +142,7 @@ void main() {
       );
       expect(launched, true);
       expect(api.usedWebView, true);
+      // expect(api.allowedCustomTab, false); // OHOS not supported
       expect(api.passedWebViewOptions?.enableDomStorage, false);
       expect(api.passedWebViewOptions?.enableJavaScript, false);
       expect(api.passedWebViewOptions?.headers, isEmpty);
@@ -176,11 +178,26 @@ void main() {
       expect(api.passedWebViewOptions?.enableDomStorage, true);
     });
 
+    // OHOS not supported
+    // test('passes showTitle to webview', () async {
+    //   final UrlLauncherOhos launcher = UrlLauncherOhos(api: api);
+    //   await launcher.launchUrl(
+    //     'http://example.com/',
+    //     const LaunchOptions(
+    //       browserConfiguration: InAppBrowserConfiguration(
+    //         showTitle: true,
+    //       ),
+    //     ),
+    //   );
+
+    //   expect(api.passedBrowserOptions?.showTitle, true);
+    // });
+
     test('passes through no-activity exception', () async {
       final UrlLauncherOhos launcher = UrlLauncherOhos(api: api);
       await expectLater(
           launcher.launch(
-            'noactivity://scheme',
+            'https://noactivity',
             useSafariVC: false,
             useWebView: true,
             enableJavaScript: false,
@@ -208,12 +225,201 @@ void main() {
     });
   });
 
-  group('closeWebView', () {
+  group('launch without webview', () {
     test('calls through', () async {
       final UrlLauncherOhos launcher = UrlLauncherOhos(api: api);
-      await launcher.closeWebView();
+      final bool launched = await launcher.launchUrl(
+        'http://example.com/',
+        const LaunchOptions(mode: PreferredLaunchMode.externalApplication),
+      );
+      expect(launched, true);
+      expect(api.usedWebView, false);
+      expect(api.passedWebViewOptions?.headers, isEmpty);
+    });
 
-      expect(api.closed, true);
+    test('passes headers', () async {
+      final UrlLauncherOhos launcher = UrlLauncherOhos(api: api);
+      await launcher.launchUrl(
+        'http://example.com/',
+        const LaunchOptions(
+            mode: PreferredLaunchMode.externalApplication,
+            webViewConfiguration: InAppWebViewConfiguration(
+                headers: <String, String>{'key': 'value'})),
+      );
+      expect(api.passedWebViewOptions?.headers.length, 1);
+      expect(api.passedWebViewOptions?.headers['key'], 'value');
+    });
+
+    test('passes through no-activity exception', () async {
+      final UrlLauncherOhos launcher = UrlLauncherOhos(api: api);
+      await expectLater(
+          launcher.launchUrl('https://noactivity', const LaunchOptions()),
+          throwsA(isA<PlatformException>()));
+    });
+
+    test('throws if there is no handling activity', () async {
+      final UrlLauncherOhos launcher = UrlLauncherOhos(api: api);
+      await expectLater(
+          launcher.launchUrl('unknown://scheme', const LaunchOptions()),
+          throwsA(isA<PlatformException>().having(
+              (PlatformException e) => e.code, 'code', 'ACTIVITY_NOT_FOUND')));
+    });
+  });
+
+  group('launch with webview', () {
+    test('calls through', () async {
+      final UrlLauncherOhos launcher = UrlLauncherOhos(api: api);
+      final bool launched = await launcher.launchUrl('http://example.com/',
+          const LaunchOptions(mode: PreferredLaunchMode.inAppWebView));
+      expect(launched, true);
+      expect(api.usedWebView, true);
+      // expect(api.allowedCustomTab, false); // OHOS not supported
+      expect(api.passedWebViewOptions?.enableDomStorage, true);
+      expect(api.passedWebViewOptions?.enableJavaScript, true);
+      expect(api.passedWebViewOptions?.headers, isEmpty);
+    });
+
+    test('passes enableJavaScript to webview', () async {
+      final UrlLauncherOhos launcher = UrlLauncherOhos(api: api);
+      await launcher.launchUrl(
+          'http://example.com/',
+          const LaunchOptions(
+              mode: PreferredLaunchMode.inAppWebView,
+              webViewConfiguration:
+                  InAppWebViewConfiguration(enableJavaScript: false)));
+
+      expect(api.passedWebViewOptions?.enableJavaScript, false);
+    });
+
+    test('passes enableDomStorage to webview', () async {
+      final UrlLauncherOhos launcher = UrlLauncherOhos(api: api);
+      await launcher.launchUrl(
+          'http://example.com/',
+          const LaunchOptions(
+              mode: PreferredLaunchMode.inAppWebView,
+              webViewConfiguration:
+                  InAppWebViewConfiguration(enableDomStorage: false)));
+
+      expect(api.passedWebViewOptions?.enableDomStorage, false);
+    });
+
+    test('passes through no-activity exception', () async {
+      final UrlLauncherOhos launcher = UrlLauncherOhos(api: api);
+      await expectLater(
+          launcher.launchUrl('https://noactivity',
+              const LaunchOptions(mode: PreferredLaunchMode.inAppWebView)),
+          throwsA(isA<PlatformException>()));
+    });
+
+    test('throws if there is no handling activity', () async {
+      final UrlLauncherOhos launcher = UrlLauncherOhos(api: api);
+      await expectLater(
+          launcher.launchUrl('unknown://scheme',
+              const LaunchOptions(mode: PreferredLaunchMode.inAppWebView)),
+          throwsA(isA<PlatformException>().having(
+              (PlatformException e) => e.code, 'code', 'ACTIVITY_NOT_FOUND')));
+    });
+  });
+
+  group('launch with custom tab', () {
+    test('calls through', () async {
+      final UrlLauncherOhos launcher = UrlLauncherOhos(api: api);
+      final bool launched = await launcher.launchUrl('http://example.com/',
+          const LaunchOptions(mode: PreferredLaunchMode.inAppBrowserView));
+      expect(launched, true);
+      expect(api.usedWebView, true);
+      // expect(api.allowedCustomTab, true); // OHOS not supported
+    });
+  });
+
+  group('launch with platform default', () {
+    test('uses custom tabs for http', () async {
+      final UrlLauncherOhos launcher = UrlLauncherOhos(api: api);
+      final bool launched = await launcher.launchUrl(
+          'http://example.com/', const LaunchOptions());
+      expect(launched, true);
+      expect(api.usedWebView, true);
+      // expect(api.allowedCustomTab, true); // OHOS not supported
+    });
+
+    test('uses custom tabs for https', () async {
+      final UrlLauncherOhos launcher = UrlLauncherOhos(api: api);
+      final bool launched = await launcher.launchUrl(
+          'https://example.com/', const LaunchOptions());
+      expect(launched, true);
+      expect(api.usedWebView, true);
+      // expect(api.allowedCustomTab, true); // OHOS not supported
+    });
+
+    test('uses external for other schemes', () async {
+      final UrlLauncherOhos launcher = UrlLauncherOhos(api: api);
+      final bool launched = await launcher.launchUrl(
+          'supportedcustomscheme://example.com/', const LaunchOptions());
+      expect(launched, true);
+      expect(api.usedWebView, false);
+    });
+  });
+
+  group('supportsMode', () {
+    test('returns true for platformDefault', () async {
+      final UrlLauncherOhos launcher = UrlLauncherOhos(api: api);
+      expect(await launcher.supportsMode(PreferredLaunchMode.platformDefault),
+          true);
+    });
+
+     // OHOS not supported,`UrlLauncherOhos`未重写`supportsMode()`，使用 `UrlLauncherPlatform` 的默认实现，仅对 `platformDefault` 返回 true，其他所有模式返回 fals
+    test(skip: true, 'returns true for external application', () async {
+      final UrlLauncherOhos launcher = UrlLauncherOhos(api: api);
+      expect(
+          await launcher.supportsMode(PreferredLaunchMode.externalApplication),
+          true);
+    });
+
+    // OHOS not supported,`UrlLauncherOhos`未重写`supportsMode()`，使用 `UrlLauncherPlatform` 的默认实现，仅对 `platformDefault` 返回 true，其他所有模式返回 fals
+    test(skip: true, 'returns true for in app web view', () async {
+      final UrlLauncherOhos launcher = UrlLauncherOhos(api: api);
+      expect(
+          await launcher.supportsMode(PreferredLaunchMode.inAppWebView), true);
+    });
+
+    // OHOS not supported,`UrlLauncherOhos`未重写`supportsMode()`，使用 `UrlLauncherPlatform` 的默认实现，仅对 `platformDefault` 返回 true，其他所有模式返回 fals
+    test(skip: true, 'returns true for in app browser view when available', () async {
+      final UrlLauncherOhos launcher = UrlLauncherOhos(api: api);
+      // api.hasCustomTabSupport = true; // OHOS not supported
+      expect(await launcher.supportsMode(PreferredLaunchMode.inAppBrowserView),
+          true);
+    });
+
+    test('returns false for in app browser view when not available', () async {
+      final UrlLauncherOhos launcher = UrlLauncherOhos(api: api);
+      // api.hasCustomTabSupport = false; // OHOS not supported
+      expect(await launcher.supportsMode(PreferredLaunchMode.inAppBrowserView),
+          false);
+    });
+  });
+
+  group('supportsCloseForMode', () {
+    test('returns true for in app web view', () async {
+      final UrlLauncherOhos launcher = UrlLauncherOhos(api: api);
+      expect(
+          await launcher.supportsCloseForMode(PreferredLaunchMode.inAppWebView),
+          true);
+    });
+
+    test('returns false for other modes', () async {
+      final UrlLauncherOhos launcher = UrlLauncherOhos(api: api);
+      expect(
+          await launcher
+              .supportsCloseForMode(PreferredLaunchMode.externalApplication),
+          false);
+      expect(
+          await launcher.supportsCloseForMode(
+              PreferredLaunchMode.externalNonBrowserApplication),
+          false);
+      expect(
+          await launcher
+              .supportsCloseForMode(PreferredLaunchMode.inAppBrowserView),
+          false);
     });
   });
 }
@@ -222,8 +428,11 @@ void main() {
 ///
 /// See _launch for the behaviors.
 class _FakeUrlLauncherApi implements UrlLauncherApi {
+  // bool hasCustomTabSupport = true; // OHOS not supported
   WebViewOptions? passedWebViewOptions;
+  // BrowserOptions? passedBrowserOptions; // OHOS not supported
   bool? usedWebView;
+  // bool? allowedCustomTab; // OHOS not supported
   bool? closed;
 
   /// A domain that will be treated as having no handler, even for http(s).
@@ -237,7 +446,11 @@ class _FakeUrlLauncherApi implements UrlLauncherApi {
   @override
   Future<bool> launchUrl(String url, Map<String?, String?> headers) async {
     passedWebViewOptions = WebViewOptions(
-        enableJavaScript: false, enableDomStorage: false, headers: headers);
+      enableJavaScript: false,
+      enableDomStorage: false,
+      headers: headers,
+    );
+
     usedWebView = false;
     return _launch(url);
   }
@@ -254,16 +467,47 @@ class _FakeUrlLauncherApi implements UrlLauncherApi {
     return _launch(url);
   }
 
+  // OHOS not supported
+  // @override
+  // Future<bool> openUrlInApp(
+  //   String url,
+  //   bool allowCustomTab,
+  //   WebViewOptions webViewOptions,
+  //   BrowserOptions browserOptions,
+  // ) async {
+  //   passedWebViewOptions = webViewOptions;
+  //   passedBrowserOptions = browserOptions;
+  //   usedWebView = true;
+  //   allowedCustomTab = allowCustomTab;
+  //   return _launch(url);
+  // }
+
+  // OHOS not supported
+  // @override
+  // Future<bool> supportsCustomTabs() async {
+  //   return hasCustomTabSupport;
+  // }
+
   bool _launch(String url) {
     final String scheme = url.split(':')[0];
     switch (scheme) {
       case 'http':
       case 'https':
+      case 'supportedcustomscheme':
+        if (url.endsWith('noactivity')) {
+          throw PlatformException(code: 'NO_ACTIVITY');
+        }
         return !url.contains(specialHandlerDomain);
-      case 'noactivity':
-        throw PlatformException(code: 'NO_ACTIVITY');
       default:
         return false;
     }
   }
+
+  @override
+  // ignore: non_constant_identifier_names
+  BinaryMessenger? get pigeonVar_binaryMessenger => null;
+
+  @override
+  // ignore: non_constant_identifier_names
+  String get pigeonVar_messageChannelSuffix => '';
 }
