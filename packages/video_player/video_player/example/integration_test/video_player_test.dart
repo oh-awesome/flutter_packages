1b18e16d34b5f9db012b4590398333dc13be2bfa
xileilei <xi_leilei@hoperun.com>
2026-06-17 15:27:15 +0800

    feat:使用单元测试和集成测试进行video_player2.10.1分支质量加固
    Signed-off-by: xileilei <xi_leilei@hoperun.com>

--- packages/video_player/video_player/example/integration_test/video_player_test.dart
+++ packages/video_player/video_player/example/integration_test/video_player_test.dart
@@ -265,7 +265,7 @@ void main() {
 
       await controller.pause();
       expect(controller.value.isPlaying, false);
-    }, skip: kIsWeb);
+    }, skip: kIsWeb || defaultTargetPlatform == TargetPlatform.ohos);
   });
 
   group('network videos', () {
@@ -313,7 +313,8 @@ void main() {
           // MEDIA_ELEMENT_ERROR on web, see https://github.com/flutter/flutter/issues/169219
           kIsWeb ||
           // Hanging on Android, see https://github.com/flutter/flutter/issues/160797
-          defaultTargetPlatform == TargetPlatform.android,
+          defaultTargetPlatform == TargetPlatform.android ||
+          defaultTargetPlatform == TargetPlatform.ohos,
     );
   });
 
