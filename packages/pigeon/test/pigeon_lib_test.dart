a24a0e3e3604fa09ee7ac50e6d046cb40054842d
xileilei <xi_leilei@hoperun.com>
2026-06-22 20:18:10 +0800

    br_pigeon-v25.3.2_ohos_dev功能补齐
    Fix ArkTS spacing and HostApi signature formatting, default ArkTS
    copyright header resolution, and Dart codec else-if to emit
    `} else if` on the same line. Restore OpenHarmony README UTF-8
    encoding and sanitize wording for compliance review. Regenerate
    example Dart and ArkTS outputs.
    
    Note: AstEventChannelApi is retained to match upstream pigeon v25.3.2.
    
    Signed-off-by: xileilei <xi_leilei@hoperun.com>

--- packages/pigeon/test/pigeon_lib_test.dart
+++ packages/pigeon/test/pigeon_lib_test.dart
@@ -463,12 +463,74 @@ abstract class NestorApi {
   test('Java generator copyright flag', () {
     final Root root = Root(apis: <Api>[], classes: <Class>[], enums: <Enum>[]);
     const PigeonOptions options = PigeonOptions(
-        javaOut: 'Foo.java', copyrightHeader: './copyright_header.txt');
+        input: 'foo.dart',
+        javaOut: 'Foo.java',
+        copyrightHeader: './copyright_header.txt');
     final JavaGeneratorAdapter javaGeneratorAdapter = JavaGeneratorAdapter();
     final StringBuffer buffer = StringBuffer();
     javaGeneratorAdapter.generate(buffer,
         InternalPigeonOptions.fromPigeonOptions(options), root, FileType.na);
     expect(buffer.toString(), startsWith('// Copyright 2013'));
+  });
+
+  test('ArkTS generator uses pigeons/copyright.txt by default', () {
+    final Root root = Root(apis: <Api>[], classes: <Class>[], enums: <Enum>[]);
+    final InternalPigeonOptions options =
+        InternalPigeonOptions.fromPigeonOptions(
+      PigeonOptions(
+        input: 'foo.dart',
+        arkTSOut: 'Foo.ets',
+        basePath: 'example/app',
+      ),
+    );
+    final ArkTSGeneratorAdapter adapter = ArkTSGeneratorAdapter();
+    final StringBuffer buffer = StringBuffer();
+    adapter.generate(buffer, options, root, FileType.na);
+    expect(
+      buffer.toString(),
+      startsWith('/*\n* Copyright (C) 2024 Huawei Device Co., Ltd.'),
+    );
+  });
+
+  test('ArkTS generator uses built-in copyright when file missing', () {
+    final Root root = Root(apis: <Api>[], classes: <Class>[], enums: <Enum>[]);
+    final InternalPigeonOptions options =
+        InternalPigeonOptions.fromPigeonOptions(
+      PigeonOptions(
+        input: 'foo.dart',
+        arkTSOut: 'Foo.ets',
+        basePath: 'nonexistent/path',
+      ),
+    );
+    final ArkTSGeneratorAdapter adapter = ArkTSGeneratorAdapter();
+    final StringBuffer buffer = StringBuffer();
+    adapter.generate(buffer, options, root, FileType.na);
+    expect(
+      buffer.toString(),
+      startsWith('/*\n* Copyright (C) 2024 Huawei Device Co., Ltd.'),
+    );
+  });
+
+  test('@ConfigurePigeon ArkTSOptions.copyrightHeader', () {
+    const String code = '''
+@ConfigurePigeon(PigeonOptions(
+  input: 'foo.dart',
+  arkTSOptions: ArkTSOptions(copyrightHeader: <String>['Custom', 'Header']),
+))
+class Message {
+  int? id;
+}
+''';
+
+    final ParseResults results = parseSource(code);
+    final PigeonOptions options = PigeonOptions.fromMap(results.pigeonOptions!);
+    final InternalPigeonOptions internalOptions =
+        InternalPigeonOptions.fromPigeonOptions(options);
+    final Root root = Root(apis: <Api>[], classes: <Class>[], enums: <Enum>[]);
+    final ArkTSGeneratorAdapter adapter = ArkTSGeneratorAdapter();
+    final StringBuffer buffer = StringBuffer();
+    adapter.generate(buffer, internalOptions, root, FileType.na);
+    expect(buffer.toString(), startsWith('/*\n* Custom\n* Header'));
   });
 
   test('Objc header generator copyright flag', () {
