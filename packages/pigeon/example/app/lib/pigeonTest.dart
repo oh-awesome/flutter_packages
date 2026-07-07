8add4e707340361299a6a85ca79fde6d679ce4d8
qq_77777 <qishijie3@h-partners.com>
2026-07-07 10:23:48 +0800

    修改版权归huawei所有
    Signed-off-by: qq_77777 <qishijie3@h-partners.com>

--- packages/pigeon/example/app/lib/pigeonTest.dart
+++ packages/pigeon/example/app/lib/pigeonTest.dart
@@ -1,6 +1,6 @@
-// Copyright 2013 The Flutter Authors. All rights reserved.
+// Copyright (c) 2025 Huawei Device Co., Ltd. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// found in the LICENSE_HW file.
 
 import 'package:pigeon/pigeon.dart';
 
